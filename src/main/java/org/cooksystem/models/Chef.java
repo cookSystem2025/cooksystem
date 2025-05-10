55831d9fbc6bec0488186b271c573b84231a933f
KaramSaffarinii <s12112080@stu.najah.edu>
2025-05-10 21:58:33 +0300

    Update Chef.java

--- src/main/java/org/cooksystem/models/Chef.java
+++ src/main/java/org/cooksystem/models/Chef.java
@@ -42,4 +42,13 @@ public class Chef {
     return input.trim();
 }
 
+    public void debugLog(String tag, Object data) {
+    if (data != null) {
+        System.out.println("[DEBUG][" + tag + "] " + data.toString());
+    } else {
+        System.out.println("[DEBUG][" + tag + "] (null)");
+    }
+}
+
+
 }
