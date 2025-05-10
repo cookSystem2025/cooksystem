0888faf57a127692bddd96ece523265f4c8eac81
KaramSaffarinii <s12112080@stu.najah.edu>
2025-05-10 21:58:15 +0300

    Update Main.java

--- src/main/java/org/cooksystem/Main.java
+++ src/main/java/org/cooksystem/Main.java
@@ -24,5 +24,13 @@ public class Main {
     return input.trim();
 }
 
+    public void debugLog(String tag, Object data) {
+    if (data != null) {
+        System.out.println("[DEBUG][" + tag + "] " + data.toString());
+    } else {
+        System.out.println("[DEBUG][" + tag + "] (null)");
+    }
+}
+
     
 }
