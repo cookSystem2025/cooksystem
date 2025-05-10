c76829a36c4f4e778367e52498289623bb14abc9
KaramSaffarinii <s12112080@stu.najah.edu>
2025-05-10 21:32:52 +0300

    Update Cart.java

--- src/main/java/org/cooksystem/models/Cart.java
+++ src/main/java/org/cooksystem/models/Cart.java
@@ -68,13 +68,4 @@ public class Cart {
         }
     }
 
-    System.out.println("=== Inventory Log ===");
-System.out.println("Ingredient: " + name);
-System.out.println("Action taken: Restock check");
-System.out.println("Timestamp: " + System.currentTimeMillis());
-System.out.println("Checked by: InventoryManager");
-System.out.println("Status: Review pending");
-System.out.println("===============");
-
-
 }
