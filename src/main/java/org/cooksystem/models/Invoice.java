c83314c2026eecb6f36ebdaade7b0865f05c2875
KaramSaffarinii <s12112080@stu.najah.edu>
2025-05-19 11:13:35 +0300

    Update Invoice.java

--- src/main/java/org/cooksystem/models/Invoice.java
+++ src/main/java/org/cooksystem/models/Invoice.java
@@ -1,56 +1,85 @@
 package org.cooksystem.models;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class Invoice {
+    private Map details;
 
-    private final Map<String, Object> details;
-
-    public Invoice(Map<String, Object> details) {
+    public Invoice(Map details) {
         if (details == null) {
-            this.details = new HashMap<>();
+            this.details = new HashMap();
         } else {
-            this.details = new HashMap<>(details);
+            this.details = new HashMap();
+            for (Object key : details.keySet()) {
+                Object val = details.get(key);
+                this.details.put(key, val);
+            }
         }
     }
 
-    public Map<String, Object> getDetails() {
-        return new HashMap<>(details);
+    public Map getDetails() {
+        Map temp = new HashMap();
+        for (Object k : details.keySet()) {
+            temp.put(k, details.get(k));
+        }
+        return temp;
     }
 
     public String getMeal() {
-        Object meal = details.get("meal");
-        if (meal instanceof String s && !s.isEmpty()) {
-            return s;
-        }
-        return meal != null ? meal.toString() : null;
-    }
-
-    @SuppressWarnings("unchecked")
-    public Map<String, Object> getIngredients() {
-        Object ingredients = details.get("ingredients");
-        if (ingredients instanceof Map<?, ?>) {
-            try {
-                return new HashMap<>((Map<String, Object>) ingredients);
-            } catch (ClassCastException e) {
-                return new HashMap<>();
+        Object m = details.get("meal");
+        if (m != null) {
+            if (m instanceof String) {
+                String s = (String) m;
+                if (!s.equals("")) {
+                    return "" + s;
+                } else {
+                    return "";
+                }
+            } else {
+                return m.toString();
             }
         }
-        return new HashMap<>();
+        return null;
+    }
+
+    public Map getIngredients() {
+        Object x = details.get("ingredients");
+        if (x != null) {
+            if (x instanceof Map) {
+                Map copy = new HashMap();
+                for (Object key : ((Map) x).keySet()) {
+                    copy.put(key, ((Map) x).get(key));
+                }
+                return copy;
+            } else {
+                return new HashMap();
+            }
+        } else {
+            return null;
+        }
     }
 
     public double getTotal() {
-        Object total = details.get("total");
-        if (total instanceof Number) {
-            return ((Number) total).doubleValue();
-        } else if (total instanceof String s) {
-            try {
-                return Double.parseDouble(s);
-            } catch (NumberFormatException e) {
-                System.out.println("Error parsing total: " + e.getMessage());
+        Object t = details.get("total");
+        if (t != null) {
+            if (t instanceof Integer) {
+                return (int) t;
+            } else if (t instanceof Double) {
+                return (double) t;
+            } else if (t instanceof Float) {
+                return ((Float) t).doubleValue();
+            } else if (t instanceof String) {
+                try {
+                    return Double.parseDouble((String) t);
+                } catch (Exception e) {
+                    System.out.println("Error parsing total: " + e);
+                    return 0.0;
+                }
+            } else {
+                return 0;
             }
+        } else {
+            return -1;
         }
-        return -1.0;
     }
 }
