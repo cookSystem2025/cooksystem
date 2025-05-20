a35643743aef35aed5e8f43df134039f5d920af1
KaramSaffarinii <s12112080@stu.najah.edu>
2025-05-20 12:20:20 +0300

    Update ChefGetsTaskAlert.java

--- src/test/java/test/Section3/ChefGetsTaskAlert.java
+++ src/test/java/test/Section3/ChefGetsTaskAlert.java
@@ -1,28 +1,31 @@
 package test.Section3;
 
 import io.cucumber.java.en.*;
+import org.cooksystem.models.Chef;
 import static org.junit.Assert.*;
+
 import java.util.HashMap;
 import java.util.Map;
 
 public class ChefGetsTaskAlert {
 
-    private Map<String, String> chefTasks = new HashMap<>();
+    private Map<Chef, String> chefTasks = new HashMap<>();
     private String notificationMessage = "";
+    private Chef notifiedChef;
 
     @Given("the chef {string} has been assigned the task {string}")
     public void the_chef_has_been_assigned_the_task(String chefName, String taskName) {
-        chefTasks.put(chefName, taskName);
-        System.out.println(chefName + " has been assigned: " + taskName);
+        notifiedChef = new Chef(chefName, "CH" + chefName.hashCode());
+        chefTasks.put(notifiedChef, taskName);
     }
 
     @When("the system sends a notification to the chef")
     public void the_system_sends_a_notification_to_the_chef() {
-        if (!chefTasks.isEmpty()) {
-            for (Map.Entry<String, String> entry : chefTasks.entrySet()) {
-                notificationMessage = "Hello " + entry.getKey() + ", your task is: " + entry.getValue();
-                System.out.println(notificationMessage);
-            }
+        for (Map.Entry<Chef, String> entry : chefTasks.entrySet()) {
+            Chef chef = entry.getKey();
+            String task = entry.getValue();
+            notificationMessage = chef.receiveTaskNotification(task);
+            break;
         }
     }
 
